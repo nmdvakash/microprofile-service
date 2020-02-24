05e0948fabf5b0afa21c66d7d1e112e67117f6d7
nmdvakash <nmdv.akash@gmail.com>
2020-02-24 23:41:27 +0530

    first commit

---
+++ src/main/java/com/example/microprofile/service/HelloController.java
@@ -0,0 +1,28 @@
+package com.example.microprofile.service;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+/**
+ *
+ */
+
+@Singleton
+@Path("/hello")
+public class HelloController {
+
+	@GET
+	public String sayHello() {
+		return "Hello World";
+	}
+
+	@Path("/test/{name}")
+
+	@GET
+	public String sayHelloForPathVariable(@PathParam(value = "name") String name) {
+		return "Hello :: " + name;
+	}
+}
---
+++ src/main/java/com/example/microprofile/service/MicroprofileserviceRestApplication.java
@@ -0,0 +1,11 @@
+package com.example.microprofile.service;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/")
+public class MicroprofileserviceRestApplication extends Application {
+}
